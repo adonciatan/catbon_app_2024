640d4baeb33cba22819a28bf6c01d3812eb57d1d
adonciatan <159789330+adonciatan@users.noreply.github.com>
2024-04-10 00:17:28 +0200

    Add files via upload

--- capp/__init__.py
+++ capp/__init__.py
@@ -7,9 +7,15 @@ import os
 application = Flask(__name__)
 
 ### Code GitHub
-application.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+# application.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 # DBVAR=os.environ['DATABASE_URL']
 # DBVAR="postgresql://username:os.environ.get(‘DB_PASSWORD’)@host:port/database"
+# DBVAR="postgresql://username:password@host:port/database"
+# application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+# application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
+
+### Code computer
+application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
 DBVAR="postgresql://u750sgbrj4rvo3:p0afbd6fe1d96c0ce3d25d516cf759d9ba4d5c6c1e428988f7680969d40acdd1d@c2dr1dq7r4d57i.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d7betnh6r04sqj"
 application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
 application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
